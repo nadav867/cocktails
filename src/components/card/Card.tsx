774f85a62fddde96a5604abf0ed21766a25a6928
Nadav Podjarski <nadavp122@gmail.com>
2024-05-16 19:24:03 +0300

    add cocktail card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -6,12 +6,5 @@ export type CardProps = {
 };
 
 export const Card = ({ children }: CardProps) => {
-  return (
-    <div className={classes.container}>
-      <div className={classes.content}>
-        <div className={classes.title}></div>
-        <div className={classes.body}>{children}</div>
-      </div>
-    </div>
-  );
+  return <div className={classes.container}>{children}</div>;
 };
--- src/features/cocktails/components/cocktail-card/CocktailCard.tsx
+++ src/features/cocktails/components/cocktail-card/CocktailCard.tsx
@@ -1,1 +1,13 @@
-export const CocktailCard = () => {};
+import { Card } from "../../../../components";
+
+type CocktailCardProp = {
+  cocktail: any;
+};
+
+export const CocktailCard = ({ cocktail }: CocktailCardProp) => {
+  return (
+    <Card>
+      <div>{cocktail.strDrink}</div>
+    </Card>
+  );
+};
