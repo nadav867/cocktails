36788c3d635a5c2d1a40e913b90d906fbe3aec18
Nadav Podjarski <nadavp122@gmail.com>
2024-05-18 12:39:26 +0300

    clenaup

--- src/features/cocktails/store/cocktial-store.tsx
+++ src/features/cocktails/store/cocktial-store.tsx
@@ -1,20 +1,12 @@
 import { create } from "zustand";
 
 type CocktailStore = {
-  searchTerm: string;
-  setSearchTerm: (value: string) => void;
   savedCocktails: Array<any>;
-  cocktails: Array<any>;
-  setCocktails: (cocktails: Array<any>) => void;
   saveCocktail: (cocktail: any) => void;
 };
 
 export const useCocktailStore = create<CocktailStore>((set) => ({
-  searchTerm: "",
-  setSearchTerm: (searchTerm) => set(() => ({ searchTerm })),
   savedCocktails: [],
-  cocktails: [],
-  setCocktails: (cocktails) => set(() => ({ cocktails })),
   saveCocktail: (cocktail) =>
     set((state) => ({ savedCocktails: [...state.savedCocktails, cocktail] })),
 }));
